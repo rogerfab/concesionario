ad51de93aa49a1fadcc54bef5ce7b36bd56c4225
Roger <ortegafabian@hotmail.com>
2015-09-07 21:10:53 -0500

    Inicio de proyecto CONEX BD

--- concesionario1/settings.py
+++ concesionario1/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'concesionario1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'concesionarios_bd'),
+        'USER': 'posgres',
+        'PASSWORD': '110378',
     }
 }
 
